6713c94ca19a54b1a5fbe96f8c892fd0e1902679
Baptiste <tabarbaptiste@gmail.com>
2025-08-12 13:00:41 +0200

    Création du module user

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { DatabaseModule } from './database/database.module';
 import { PrestationModule } from './prestation/prestation.module';
 import { CategoryModule } from './category/category.module';
 import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [DatabaseModule, PrestationModule, CategoryModule, ProductModule],
+  imports: [DatabaseModule, PrestationModule, CategoryModule, ProductModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
